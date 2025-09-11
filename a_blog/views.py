7b3e3ab61d519e1739abe486ef49ef16bcb0a9d0
Leaders2022 <georgeobby@gmail.com>
2025-09-11 16:26:43 +0300

    first commit

---
+++ a_blog/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def blog(request):
+    return render(request, 'a_blog/blog.html')
+
+def post(request):
+    return render(request, 'a_blog/post.html')

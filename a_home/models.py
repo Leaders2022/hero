7b67425bd9bbd8e76cb639ba8cdf23cb9a9d7c60
Leaders2022 <georgeobby@gmail.com>
2025-11-30 10:32:44 +0000

    second commit

--- a_home/models.py
+++ a_home/models.py
@@ -1,3 +1,88 @@
 from django.db import models
+from wagtail.snippets.models import register_snippet 
+from wagtail.admin.panels import FieldPanel, MultiFieldPanel
+from wagtail.models import Page
+from wagtail.fields import StreamField
+from wagtail import blocks
+from wagtail.images.blocks import ImageChooserBlock
+ 
 
-# Create your models here.
+@register_snippet
+class NavigationItem(models.Model):
+    title = models.CharField(max_length=255)
+    link = models.CharField(max_length=500, help_text="Use absolute path (eg /about/) or full external URL")
+    sort_order = models.IntegerField(default=0)
+
+    panels = [
+        FieldPanel('title'),
+        FieldPanel('link'),
+        FieldPanel('sort_order'),
+    ]
+    class Meta:
+        ordering = ['sort_order']
+  
+
+    def __str__(self):
+        return self.title
+
+@register_snippet
+class SiteHeader(models.Model):
+    logo = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    tagline = models.CharField(max_length=255, blank=True)
+
+    panels = [
+        FieldPanel('logo'),
+        FieldPanel('tagline'),
+    ]
+
+    def __str__(self):
+        return "Site Header"    
+
+class FeatureBlock(blocks.StructBlock):
+    title = blocks.CharBlock(required=True)
+    description = blocks.TextBlock(required=False)
+    icon = ImageChooserBlock(required=False)
+
+class CourseBlock(blocks.StructBlock):
+    title = blocks.CharBlock(required=True)
+    excerpt = blocks.TextBlock(required=False)
+    date = blocks.DateBlock(required=False)
+    image = ImageChooserBlock(required=False)
+    link = blocks.URLBlock(required=False)
+    price = blocks.CharBlock(required=False, help_text="e.g., $49.99")
+    reviews_count = blocks.IntegerBlock(required=False, help_text="Number of reviews")
+
+class RegisterBlock(blocks.StructBlock):
+    heading = blocks.CharBlock()
+    cta_text = blocks.CharBlock()
+    cta_link = blocks.URLBlock()
+
+class ProfileBlock(blocks.StructBlock):
+    name = blocks.CharBlock()
+    role = blocks.CharBlock(required=False)
+    image = ImageChooserBlock(required=False)
+    bio = blocks.TextBlock(required=False)
+
+class TrustBlock(blocks.StructBlock):
+    logo = ImageChooserBlock()
+    name = blocks.CharBlock()
+
+class HomePage(Page):
+    template = "a_home/index.html"   # use your existing template or create a new home template
+    body = StreamField([
+        ('features', blocks.ListBlock(FeatureBlock())),
+        ('courses', blocks.ListBlock(CourseBlock())),
+        ('registration', RegisterBlock()),
+        ('profiles', blocks.ListBlock(ProfileBlock())),
+        ('trusted_by', blocks.ListBlock(TrustBlock())),
+    ], null=True, blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body'),
+    ]
--- hero/urls.py
+++ hero/urls.py
@@ -1,24 +1,16 @@
-"""
-URL configuration for hero project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+from wagtail.admin import urls as wagtailadmin_urls
+from wagtail.documents import urls as wagtaildocs_urls
+from wagtail.images import urls as wagtailimages_urls
+from wagtail import urls as wagtail_urls   
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('django-admin/', admin.site.urls),
+    path('admin/', include(wagtailadmin_urls)),
+    path('documents/', include(wagtaildocs_urls)),
+    path('images/', include(wagtailimages_urls)),
     path('', include('a_home.urls')),
     path('blog/', include('a_blog.urls')),
+    path('', include(wagtail_urls)),
 ]
